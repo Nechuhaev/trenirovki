183bbe492b7e7b24265bc4a85b68e9379e265d87
Nechuhaev <43204291+Nechuhaev@users.noreply.github.com>
2022-06-24 14:35:42 +0000

    Deploying to gh-pages from @ Nechuhaev/trenirovki@5e4b2372b8beee7a763da0407a7e0f9cd8f3488c 🚀

---
+++ index.02400209.js
@@ -0,0 +1,2 @@
+var l,o,c;console.log("Лог до вызова функции multiply"),l=2,o=3,c=5,console.log("Результат умножения равен "+l*o*c),console.log("Лог после вызова функции multiply");
+//# sourceMappingURL=index.02400209.js.map
