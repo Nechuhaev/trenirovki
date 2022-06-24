9c7298e34f5439019a62b6ad5fa3ae2f1707d012
Alexandr Nechuhaev <a.nechuhaev@gmail.com>
2022-06-24 16:20:57 +0300

    JS1

---
+++ src/index.js
@@ -0,0 +1,61 @@
+// const clients = ["Mango", "Poly", "Ajax"];
+
+// Указывая в скобках индекс элемента мы получаем его значение
+// console.log(clients[0]); // Mango
+// console.log(clients[1]); // Poly
+// console.log(clients[2]); // Ajax
+
+// clients[0] = "Kiwi";
+// clients[1] = "Pango";
+// console.log(clients); // ["Kiwi", "Pango", "Ajax"]
+
+// for (let i = 0; i < clients.length; i += 1) {
+//   console.log(clients[i]);
+// }
+
+// const clients = ["Mango", "Ajax", "Poly"];
+
+// for (const client of clients) {
+//   console.log(client);
+// }
+
+// const string = "javascript";
+
+// for (const character of string) {
+//   console.log(character);
+// }
+
+// const clients = ["Mango", "Poly", "Ajax"];
+// const clientNameToFind = "Ajax";
+// let message;
+
+// for (const client of clients) {
+//   // На каждой итерации будем проверять совпадает ли элемент массива с
+//   // именем клиента. Если совпадает то мы записываем в message сообщение
+//   // об успехе и делаем break чтобы не искать дальше
+//   if (client === clientNameToFind) {
+//     message = "Клиент с таким именем есть в базе данных!";
+//     break;
+//   }
+
+//   // Если они не совпадают то запишем в message сообщение об отсутствии имени
+//   message = "Клиента с таким именем нету в базе данных!";
+// }
+
+// console.log(message); // "Клиент с таким именем есть в базе данных!"
+
+// const numbers = [1, 3, 14, 18, 4, 7, 29, 6, 34];
+// const threshold = 15;
+
+// // Для чисел меньше чем порог срабатывает continue, выполнение тела прекращается
+// // и управление передаётся на следующую итерацию.
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] < threshold) {
+//     continue;
+//   }
+
+//   console.log(`Число больше чем ${threshold}: ${numbers[i]}`); // 18, 29, 34
+// }
+
+// const name = "Mango";
+// console.log(name.split("")); // ["M", "a", "n", "g", "o"]
